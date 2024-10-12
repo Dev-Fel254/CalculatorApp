eb1506dad012c30332de135ff30de65beea7fbd9
unknown <odhisfello99@gmail.com>
2024-10-13 02:13:46 +0300

    Create an operate function that calls one of the Math operations

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const divide = function(a, b){
     return a/b;
 }
 
+const operate = function(operator, a, b){
+    switch(operator){
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return "Invalid operator";
+    }
+}
+
+let firstNumber = 3;
+let operator ="+";
+let secondNumber = 5;
+
+let result = operate(operator, firstNumber, secondNumber);
+console.log("Result: " + result);
 
 
 
