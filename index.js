8703eff4d72da20d6da3e4c7f40ad11dca083610
unknown <odhisfello99@gmail.com>
2024-10-13 04:08:24 +0300

    Populate the display when the numbers/operators are clicked

--- index.js
+++ index.js
@@ -34,5 +34,24 @@ let result = operate(operator, firstNumber, secondNumber);
 console.log("Result: " + result);
 
 
+const display = document.getElementById("display");
+
+const digits = document.getElementsByClassName("digit");
+const operators = document.getElementsByClassName("operator");
+
+for(let i = 0; i<digits.length; i++){
+    digits[i].addEventListener("click", function(){
+        display.value += digits[i].getAttribute('data-value');
+        console.log('Digit clicked:', digits[i].getAttribute('data-value')); 
+    })
+}
+
+for(let i = 0; i<operators.length; i++){
+    operators[i].addEventListener("click", function(){
+        display.value += operators[i].getAttribute('data-value');                                                                                                                                                  
+        console.log('Operator clicked:', operators[i].getAttribute('data-value'));
+    })
+}
+
 
 
